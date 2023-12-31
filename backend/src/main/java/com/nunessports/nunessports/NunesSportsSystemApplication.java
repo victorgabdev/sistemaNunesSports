44a3f5e2aa8721451041302c234f3cc20df7dd69
Victor Gabriel <victorgabdev@gmail.com>
2023-12-31 13:24:29 -0300

    Implemented to the initial API structure in the backend

---
+++ backend/src/main/java/com/nunessports/nunessports/NunesSportsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nunessports.nunessports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunesSportsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunesSportsSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nunessports/nunessports/NunesSportsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunessports.nunessports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunesSportsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
